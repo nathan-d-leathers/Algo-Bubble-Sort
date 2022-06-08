97ce7f8aa2b1143095204809c8eae3cb779e4f60
Nathan D Leathers <nathan.d.leathers@icloud.com>
2022-06-08 15:07:14 -0700

    working py code

--- python/bubble_sort.py
+++ python/bubble_sort.py
@@ -1,7 +1,24 @@
-sequence = [4, 3, 5, 0, 1]
-swaps = 0
+# sequence = [4, 3, 5, 0, 1]
+# swaps = 0
 
-# Your Code Here
+def bubble(sequence):
+    seq_len = len(sequence)
+    swaps = 0
+    i = 0
+    while i < seq_len - 1:
+        j = 0
+        while j < seq_len - 1 - i:
+            if sequence[j] > sequence[j + 1]:
+                sequence[j], sequence[j + 1] = sequence[j + 1], sequence[j]
+                swaps += 1
+            j += 1
+        i += 1
+    result = sequence
+    print(f"Final result: {result}")
+    print(f"Swaps: {swaps}")
 
-print(f"Final result: {result}")
-print(f"Swaps: {swaps}")
+bubble([4, 3, 5, 0, 1])
+# Final result: [0, 1, 3, 4, 5]
+# Swaps: 7
+
+ 

69fea6a126bdb9d431989ad6fd2ea603f1abe650
Nathan D Leathers <nathan.d.leathers@icloud.com>
2022-06-08 15:30:22 -0700

    solved in js

--- js/bubbleSort.js
+++ js/bubbleSort.js
@@ -1,7 +1,26 @@
-var sequence = [4, 3, 5, 0, 1]
-var swaps = 0
+const bubble = (arr) => {
+    let swaps = 0;
+    let result;
+    let arrLen = arr.length-1
+    for (let i=0; i<arrLen; i++) {
+        for(let j=0; j< arrLen-i;j++) {
+            if (arr[j] > arr[j+1]) {
+              let moveRight = arr[j+1]
+              let moveLeft = arr[j]
+              arr[j] = moveRight
+              arr[j+1] = moveLeft
+              swaps+=1
+            }
+        }
+    }
 
-// Your Code Here
+    result = arr;
+    console.log(`Final result: ${result} \nSwaps: ${swaps}`)
+}
 
-console.log("Final result: " + result)
-console.log("Swaps: " + swaps)
+let sequence = [4, 3, 5, 0, 1]
+bubble(sequence)
+
+// output: 
+// Final result: 0,1,3,4,5
+// Swaps: 7
